2b0f84e726aa5c3cd9db82f8f5d44f5073ed1403
Xchr <195504996@qq.com>
2015-06-04 15:27:56 +0100

    Test message size 64KB-256KB
    1. Add size of 64KB, 96KB, 128KB, 192KB, 256KB and 75,000

--- server.js
+++ server.js
@@ -58,9 +58,12 @@ wsserver.on('request',function(request) {
 		
 			//generate array for testing different offloading size
 			var msg1d2 = random(1200), msg2d4 = random(2400), msg3d6 = random(3600), msg4d8 = random(4800), msg6d0 = random(6000), msg7d2 = random(7200), msg8d4 = random(8400), msg9d6 = random(9600), msg10d8 = random(10800), msg12d0 = random(12000);
-		
+			
+			//generate array of 64KB, 96KB, 128KB, 192KB, 256KB and 75,000
+			var msg64k = random(12773), msg96k = random(18234),  msg128k = random(23696),  msg192k = random(34618),  msg256k = random(45541),  msg75 = random(75000);
+			
 			//store the arrays to be sent
-			var msgs = [ msg10, msg20, msg30, msg40, msg50, msg60, msg70, msg80, msg90, msg100, msg1d2, msg2d4, msg3d6, msg4d8, msg6d0, msg7d2, msg8d4, msg9d6, msg10d8, msg12d0];
+			var msgs = [ msg10, msg20, msg30, msg40, msg50, msg60, msg70, msg80, msg90, msg100, msg1d2, msg2d4, msg3d6, msg4d8, msg6d0, msg7d2, msg8d4, msg9d6, msg10d8, msg12d0, msg64k, msg96k, msg128k, msg192k, msg256k, msg75];
 			var onlyrec = msgs[obj.value];
 			connection.send(JSON.stringify(onlyrec));
 		}else{
